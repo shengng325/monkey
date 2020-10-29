d4f534869d5a66a440021c30401cc4af1bd627f3
shengng325 <shengng325@gmail.com>
2020-10-29 16:27:22 +0800

    added env to store variables

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/shengng325/monkey/evaluator"
 	"github.com/shengng325/monkey/lexer"
+	"github.com/shengng325/monkey/object"
 	"github.com/shengng325/monkey/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -31,7 +33,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
